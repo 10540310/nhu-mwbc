68c05f03e785c0ea8f8f3a42d3bd5f63e1add4ca
小幽 <64883358+10540310@users.noreply.github.com>
2026-09-09 19:31:37 +0800

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 console.log("南華中智佛學社網站啟動成功");
+
+let slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+function showSlide(index){
+
+    slides.forEach(slide=>{
+        slide.classList.remove("active");
+    });
+
+    slides[index].classList.add("active");
+}
+
+function nextSlide(){
+
+    currentSlide++;
+
+    if(currentSlide >= slides.length){
+        currentSlide = 0;
+    }
+
+    showSlide(currentSlide);
+}
+
+function prevSlide(){
+
+    currentSlide--;
+
+    if(currentSlide < 0){
+        currentSlide = slides.length - 1;
+    }
+
+    showSlide(currentSlide);
+}
+
+document.querySelector(".next").addEventListener("click", nextSlide);
+document.querySelector(".prev").addEventListener("click", prevSlide);
+
+setInterval(nextSlide, 5000);
